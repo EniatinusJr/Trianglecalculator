eb8097a31e10c2597237c42a49f05a81c81eb4f5
Maxi <46729527+Maxi03milian@users.noreply.github.com>
2021-11-29 17:06:40 +0100

    added rounding

--- triangleapp/src/store/index.js
+++ triangleapp/src/store/index.js
@@ -13,22 +13,22 @@ export default createStore({
   },
   mutations: {
     updateSeiteA (state, num) {
-      state.calcData.seiteA = num;
+      state.calcData.seiteA = num.toFixed(2);
     },
     updateSeiteB (state, num) {
-      state.calcData.seiteB = num;
+      state.calcData.seiteB = num.toFixed(2);
     },
     updateSeiteC (state, num) {
-      state.calcData.seiteC = num;
+      state.calcData.seiteC = num.toFixed(2);
     },
     updateWinkelA (state, num) {
-      state.calcData.winkelA = num;
+      state.calcData.winkelA = num.toFixed(2);
     },
     updateWinkelB (state, num) {
-      state.calcData.winkelB = num;
+      state.calcData.winkelB = num.toFixed(2);
     },
     updateWinkelC (state, num) {
-      state.calcData.winkelC = num;
+      state.calcData.winkelC = num.toFixed(2);
     },
   },
   actions: {
