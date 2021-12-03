9ec5be1e6c76679f8246399b71a3179baed584e6
Maurin Schucan <maurin@schucan.ch>
2021-12-03 15:04:33 +0100

    Created hometext

--- triangleapp/src/views/Home.vue
+++ triangleapp/src/views/Home.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <article>
-      <h2>Text</h2>
-      <p>TextTextTextTextTextText</p>
+      <h1>Trigonometrischer Rechner</h1>
+      <p>Dies ist ein Rechner für trigonometrische Berechnungen</p>
+      <h2>Funktionsweise</h2>
+      <p>Dieser Rechner berechnet alle Seiten und Winkel <br>mit Hilfe von Sinus, Cosinus Tangens und Pythagoras.</p>
+      <p>Ebenfalls soll der Rechner im stande sein herauszufinden,<br> ob das Dreieck spitzwinklig oder stumpfwinklig ist</p>
+      <h2>Programmiersprache</h2>
+      <p>Wir haben uns für Vue.js entschieden. Grund dafür war, dass alle <br> Applikationsentwickler in der Gruppe bereits damit vertraut sind und der Input in Vue.js <br> einfach leserlich ist verglichen zu anderen JavaScript Frameworks</p>
+      <br>
+      <img src="../assets/logo.png" alt="Vue logo"/>
     </article>
   </div>
 </template>
